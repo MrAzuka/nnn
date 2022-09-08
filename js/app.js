f05d18ac0f576fc83a7ed5401722466b91bb2cb2
Zuka-Dev <danazuka2003@gmail.com>
2022-09-08 11:36:04 +0100

    Completed most part

--- js/app.js
+++ js/app.js
@@ -1,51 +1,10 @@
 let hamburger = document.querySelector(".hamburger");
 let listGroup = document.querySelector(".list-group");
-let subscribeBtn = document.querySelector("#subscribe");
-let subscribed = document.querySelector(".modal");
 
 hamburger.addEventListener("click", mobileView);
-subscribeBtn.addEventListener("click", subscribe);
 
 function mobileView() {
   hamburger.classList.toggle("active");
   listGroup.classList.toggle("active");
 }
-function subscribe(e) {
-  e.preventDefault();
-  subscribed.style.visibility = "visible";
-  subscribeBtn.style.display = "none";
-  setTimeout(() => {
-    subscribeBtn.style.visibility = "hidden";
-    subscribed.style.display = "none";
-    window.location.reload();
-  }, 3000);
-}
 
-// function countdown() {
-//   const today = new Date();
-//   var offerDate = new Date("03 jul 2022");
-//   //If offer ends reset to new value
-//   if (today.getSeconds() == offerDate.getSeconds()) {
-//     offerDate = resetOfferDate();
-//   }
-//   //offerTime will have the total millseconds
-//   const offerTime = offerDate - today;
-//   // 1 sec= 1000 ms
-//   // 1 min = 60 sec
-//   // 1 hour = 60 mins
-//   // 1 day = 24 hours
-//   const offerDays = Math.floor(offerTime / (1000 * 60 * 60 * 24));
-//   const offerHours = Math.floor((offerTime / (1000 * 60 * 60)) % 24);
-//   const offerMins = Math.floor((offerTime / (1000 * 60)) % 60);
-//   const offerSecs = Math.floor((offerTime / 1000) % 60);
-//   document.querySelector(".one").innerText = offerDays;
-//   document.querySelector(".two").innerHTML = offerHours;
-//   document.querySelector(".three").innerHTML = offerMins;
-//   document.querySelector(".four").innerHTML = offerSecs;
-// }
-// function resetOfferDate() {
-//   const futureDate = new Date();
-//   futureDate.setDate(futureDate.getDate() + 15);
-//   return futureDate;
-// }
-// setInterval(countdown, 1000);
